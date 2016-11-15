89b63998cc30ef4e1ab366a6c5b8ccaa1c5e10c5
felixchen-fc <felix.chen@missionsky.com>
2016-11-15 09:50:03 +0800

    upload

---
+++ PP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PP.Startup))]
+namespace PP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
